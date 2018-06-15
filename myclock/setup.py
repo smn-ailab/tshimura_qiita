c2f650263cbc9ba2c6143b1c0ccf49a039ed2855
shimura2017 <takashi_shimura@so-netmedia.jp>
2018-06-15 16:20:35 +0900

    Added create_config.

--- myclock/setup.py
+++ myclock/setup.py
@@ -1,9 +1,17 @@
 
 from setuptools import find_packages, setup
 
+from myclock.command import create_config
+
 setup(
-    name="my_command",
+    name="myclock",
     packages=find_packages(),
+    install_requires=[
+        "arrow"],
+    # myclock コマンドはここで設定.
     entry_points={
-        'console_scripts': ['my_command=my_command.command_line:main']}
+        'console_scripts': ['myclock=myclock.command:command']}
 )
+
+# 設定ファイルをホームディレクトリに生成する.
+create_config()
